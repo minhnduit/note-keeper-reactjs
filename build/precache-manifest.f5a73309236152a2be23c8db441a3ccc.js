69fc23fd76d1d045d571ab4324292c80b57bd817
Jamie Nguyen <17520755@gm.uit.edu.vn>
2022-03-14 13:39:56 +0700

    first commit

---
+++ build/precache-manifest.f5a73309236152a2be23c8db441a3ccc.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388775d89ff6b07cf1ef2952cadfa893",
+    "url": "/minhnduit/note-keeper-reactjs/index.html"
+  },
+  {
+    "revision": "91dcddfb80aa5ef12447",
+    "url": "/minhnduit/note-keeper-reactjs/static/js/2.32b9d141.chunk.js"
+  },
+  {
+    "revision": "4ebc66b81ed302f4da78",
+    "url": "/minhnduit/note-keeper-reactjs/static/js/main.1a6075e9.chunk.js"
+  },
+  {
+    "revision": "f23dcecd4e4bae5772c2",
+    "url": "/minhnduit/note-keeper-reactjs/static/js/runtime-main.6e504fbd.js"
+  }
+]);
