813ca0e0e962589f5de231360842a70f7ba06567
Jamie Nguyen <17520755@gm.uit.edu.vn>
2022-03-14 13:22:51 +0700

    first commit

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+
+function CreateArea(props) {
+const [note, setNote] = useState({
+  title:"",
+  content:""
+})
+
+function handleChange(e){
+  const {name,value}=e.target;
+
+
+  setNote(x=>{
+    return {
+      ...x,
+      [name]: value
+    };
+  });
+}
+
+function submitNote(e){
+  props.onAdd(note);
+  setNote({
+    title:"",
+    content:""
+  })
+e.preventDefault();
+}
+
+  return (
+    <div>
+      <form>
+        <input onChange={handleChange} name="title" placeholder="Title" value={note.title} />
+        <textarea onChange={handleChange} name="content" value={note.content} placeholder="Take a note..." rows="3" />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
